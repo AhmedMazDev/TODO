7764cfd1a5d6e9d2ae3d2efe05909346ee72558b
Ahmed Maz <ahmedmazdev@gmail.com>
2021-09-05 16:56:02 +0100

    Finished

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const checkBtn = document.getElementById("check");
 const removeBtn = document.getElementById("remove");
 const form = document.getElementById("form");
 const todos = document.getElementById("todos");
+const clearBtn = document.getElementById("clear");
 
 let todosList = [];
 
+//Load event
 document.addEventListener("DOMContentLoaded", () => {
   if (!localStorage.getItem("todos")) return;
 
   const storageTodos = JSON.parse(localStorage.getItem("todos"));
   todosList = [...storageTodos];
   for (const todo of storageTodos) {
-    renderToDOM(todo.text);
+    renderToDOM(todo);
   }
 });
 
+//Form event
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -26,20 +29,28 @@ form.addEventListener("submit", (e) => {
     checked: false,
   };
 
-  renderToDOM(todoInput.value);
+  renderToDOM(todoData);
 
   todosList.push(todoData);
   localStorage.setItem("todos", JSON.stringify(todosList));
+
+  todoInput.value = "";
 });
 
-function renderToDOM(todoText) {
+function renderToDOM(todoObject) {
   const todo = document.createElement("div");
-  todo.innerHTML = `<div class="todo">
-                      <button id="check" class="left-btn">
+  todo.innerHTML = `<div id='${todoObject.id}' class="todo ${
+    todoObject.checked ? "checked" : ""
+  }">
+                      <button id="check" onclick="checkTodo('${
+                        todoObject.id
+                      }')" class="left-btn">
                       <i class="fas fa-check"></i>
                       </button>
-                      <div class="todo-text">${todoText}</div>
-                      <button id="remove" class="right-btn">
+                      <div class="todo-text">${todoObject.text}</div>
+                      <button id="remove" onclick="removeTodo('${
+                        todoObject.id
+                      }')" class="right-btn">
                         <i class="fas fa-trash"></i>
                       </button>
                       </div> `;
@@ -47,6 +58,35 @@ function renderToDOM(todoText) {
   //appending the div
   todos.appendChild(todo);
 }
+
+//removeTodo
+function removeTodo(id) {
+  const index = todosList.findIndex((item) => item.id == id);
+  todosList.splice(index, 1);
+  localStorage.setItem("todos", JSON.stringify(todosList));
+
+  const div = document.getElementById(id);
+  div.classList.add("slideRight");
+  setTimeout(() => {
+    div.style.display = "none";
+  }, 400);
+}
+
+//checkTodo
+function checkTodo(id) {
+  const index = todosList.findIndex((item) => item.id == id);
+  todosList[index].checked = true;
+  localStorage.setItem("todos", JSON.stringify(todosList));
+
+  const div = document.getElementById(id);
+  div.classList.add("checked");
+}
+
+//clear Button
+clearBtn.addEventListener("click", () => {
+  localStorage.removeItem("todos");
+  todos.innerHTML = "";
+});
 
 function generateID(length) {
   var result = "";
